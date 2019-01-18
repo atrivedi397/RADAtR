342a4fe2b048f194efd9a258c22b7ba143931b31
Dev Vrat <itsdvvs.18@gmail.com>
2019-01-18 15:44:04 +0530

    Completed check_teacher_availability function definition

--- Detached/Global/Functions/TeacherManagement.py
+++ Detached/Global/Functions/TeacherManagement.py
@@ -1,12 +1,40 @@
 from Detached.Global.Configurations.ConnectionEstablishment import *
+from Detached.Global.Variables.varFile1 import totalWorkingDays, maximumSlots
 import json
 
 
-# Here first argument "Teacher" (the class name) is passed to verify the given teacher's credentials
-# Also so that the first argument is of Teacher's instance
-def check_teacher_availability(teacher_uid):
-    details = get_teacher_details(teacher_uid)
-    pass
+# Function that checks if a teacher could be available for teaching a subject or not.
+# Arguments passed for this function is in respect to each individual teacher.
+def check_teacher_availability(free_slots_each_day, subject_lecture_per_week, max_lecture_per_day, max_lecture_of_teacher):
+    # summation of free lectures of each day (Monday + Tuesday + ... + Sunday)
+    total_no_of_free_lectures = 0
+
+    for i in range(len(free_slots_each_day)-1):
+        total_no_of_free_lectures += free_slots_each_day[i]
+
+    max_lectures = totalWorkingDays * maximumSlots                          # total no of lectures, possible in a week
+    current_no_of_lectures = max_lectures - total_no_of_free_lectures       # total no of lectures taken by the teacher
+
+    i = 0                                                                   # reinitializing i for list indexing
+    lectures_left = subject_lecture_per_week                                # for better code readability
+    while current_no_of_lectures <= max_lecture_of_teacher and lectures_left >= 0:
+        if free_slots_each_day[i] >= max_lecture_per_day:
+            """ if a teacher has more free lectures to teach the same subject in a day,
+                then the maximum no of lectures (as per 'each day' constraint) could be assign
+                for the same day"""
+            lectures_left -= max_lecture_per_day
+            i += 1                                                          # to iterate for the next day
+        else:
+            """ if a teacher has less or no free lectures to teach the same subject in a day,
+                then the maximum no of lectures (that can be taken/assigned for the same subject)
+                would be equals to no. of free slots available on that day"""
+            lectures_left -= free_slots_each_day[i]
+            i += 1                                                          # to iterate for the next day
+
+    if lectures_left <= 0:
+        return True                                                         # return 'true' if teacher is available
+    else:
+        return False                                                        # return 'false' for non availability
 
 
 # Function to get details of the teacher for a given uID
