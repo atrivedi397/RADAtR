f6364b98a42b25ad5d586f94dd19428691faf201
Dev Vrat <itsdvvs.18@gmail.com>
2019-01-19 02:11:07 +0530

    Created a temporary window for adding a course

--- GUI/IndividualWidgets/GUI_Course_Add.py
+++ GUI/IndividualWidgets/GUI_Course_Add.py
@@ -1,109 +1,58 @@
 import sys
 from PyQt5.QtWidgets import *
-import sys
-from PyQt5.QtWidgets import *
 
 
-class AddingFunctionality(QWidget):
+class AddCourse(QWidget):
     def __init__(self, parent=None):
         super().__init__(parent)
-        self.MainWindow()
 
-    def MainWindow(self):
-        self.setFixedSize(600, 340)
-        self.setGeometry(300, 90, 600, 400)
-        self.setWindowTitle(" UPDATION OF TEACHER'S DATABASE")
+        # window properties
+        self.setFixedSize(600, 400)
+        self.setWindowTitle("Add Course")
 
-        LabelMainWindow = QLabel("PLEASE  ENTER  THE  DETAILS  OF  THE  TEACHERS", self)
-        LabelMainWindow.move(120, 2)
-        LabelMainWindow.setStyleSheet("font-weight: bold;"
-                                      "font-family: Times New Roman ;"
-                                      " color: black ; "
-                                      "font-size: 10pt")
-        add = QPushButton("ADD",self)
-        add.move(200,290)
+        # layouts for the window
+        self.mainVLayout = QVBoxLayout(self)
+        self.nestedHLayout = QHBoxLayout(self)
+        self.nestedFormLayout = QFormLayout(self)
 
-        # namefield
+        # ----------- widgets of the window ---------------
+        main_prompt = QLabel("Enter the details of the course to be added.")
 
-        nameField = QLabel("NAME",self)
-        nameField.move(70,70)
-        self.TextnameField = QTextEdit(self)
-        self.TextnameField.move(250,60)
-        self.TextnameField.setFixedWidth(200)
-        self.TextnameField.setFixedHeight(30)
+        self.nameField = QLineEdit()                # for 'name' input
+        self.nameField.setFixedWidth(300)
+        self.codeField = QLineEdit()
+        self.codeField.setFixedWidth(100)
+        self.semesterField = QComboBox()
+        self.semesterField.addItems(["1", "2", "3", "4", "5", "6", "7", "8"])
+        self.semesterField.setFixedWidth(50)
+        self.startTimingField = QLineEdit()
+        self.startTimingField.setFixedWidth(100)
 
-        AddNew = QPushButton("+", self)
-        AddNew.move(458, 68)
-        AddNew.setFixedHeight(20)
-        AddNew.setFixedWidth(20)
-        AddNew.clicked.connect(self.texteditclear)
+        # adding widgets into the form layout
+        self.nestedFormLayout.addRow(QLabel("Name of the course"), self.nameField)
+        self.nestedFormLayout.addRow(QLabel("Code for the course"), self.codeField)
+        self.nestedFormLayout.addRow(QLabel("Total Semesters"), self.semesterField)
+        self.nestedFormLayout.addRow(QLabel("Starting Time (24-hours format)"), self.startTimingField)
 
-        #code
-        Code = QLabel("CODE", self)
-        Code.move(70, 110)
-        self.TextCode = QTextEdit(self)
-        self.TextCode.move(250, 110)
-        self.TextCode.setFixedWidth(200)
-        self.TextCode.setFixedHeight(30)
+        # adding form into horizontal layout (so it could be in center)
+        self.nestedHLayout.addStretch(1)                                # for space on the left
+        self.nestedHLayout.addLayout(self.nestedFormLayout)             # displaying form in the center
+        self.nestedHLayout.addStretch(1)                                # for space on the right
 
-        AddNew3 = QPushButton("+", self)
-        AddNew3.move(458, 115)
-        AddNew3.setFixedHeight(20)
-        AddNew3.setFixedWidth(20)
-        AddNew3.clicked.connect(self.textedituidclear)
-
-
-        # number of semesters
-        SemNos = QLabel("NUMBER OF SEMESTERS", self)
-        SemNos.move(70, 160)
-        self.TextSemNos = QTextEdit(self)
-        self.TextSemNos.move(250, 160)
-        self.TextSemNos.setFixedWidth(200)
-        self.TextSemNos.setFixedHeight(30)
-
-        AddNew5 = QPushButton("+", self)
-        AddNew5.move(458, 165)
-        AddNew5.setFixedHeight(20)
-        AddNew5.setFixedWidth(20)
-        AddNew5.clicked.connect(self.textSemNos)
-
-        # start timing
-        StartTiming = QLabel("MINIMUM LECTURES", self)
-        StartTiming.move(70, 210)
-        self.TextStartTiming = QTextEdit(self)
-        self.TextStartTiming.move(250, 210)
-        self.TextStartTiming.setFixedWidth(200)
-        self.TextStartTiming.setFixedHeight(30)
-
-        AddNew5 = QPushButton("+", self)
-        AddNew5.move(458, 215)
-        AddNew5.setFixedHeight(20)
-        AddNew5.setFixedWidth(20)
-        AddNew5.clicked.connect(self.textStartTiming)
-
-    def texteditclear(self):
-        # write the code to add the text of the text edit to the database
-        self.TextnameField.clear()
-
-    def textedituidclear(self):
-        # write the code to add the text of the text edit to the database
-        self.TextCode.clear()
-
-    def Comboboxdesigclear(self):
-        self.teacher_designation.setCurrentIndex(-1)
-
-    def textSemNos(self):
-        self.TextSemNos.clear()
-
-    def textStartTiming(self):
-        self.TextStartTiming.clear()
+        # stacking all the widgets in the main layout
+        self.mainVLayout.addWidget(main_prompt)
+        self.mainVLayout.addStretch(1)
+        self.mainVLayout.addLayout(self.nestedHLayout)
+        self.mainVLayout.addStretch(2)
+        self.setLayout(self.mainVLayout)
 
 
 def main():
-    ObjQApplication = QApplication(sys.argv)
-    ObjAddingFunctionality = AddingFunctionality()
-    ObjAddingFunctionality.show()
-    sys.exit(ObjQApplication.exec_())
+    application = QApplication(sys.argv)
+    application.setStyle('Fusion')
+    add_course_object = AddCourse()
+    add_course_object.show()
+    sys.exit(application.exec_())
 
 
 if __name__ == '__main__':
