3db7027629858ba1eeae0e3c6443e0060994477f
Dev Vrat <itsdvvs.18@gmail.com>
2019-02-07 23:49:01 +0530

    generation of temporary list of shared subject between teachers is done

--- GUI/GUI_TimeTable_Create_MapWindow.py
+++ GUI/GUI_TimeTable_Create_MapWindow.py
@@ -6,10 +6,16 @@ class MapWindow:
     def __init__(self, parent=None):
         # dependencies
         self.teachers = ["None", "Shared", "Manoj Kumar", "Narendra Kumar", "Sanjay Kumar Dwivedi", "Vipin Saxena", "Deepa Raj", "Shalini Chandra"]
-        self.teacher_combo_boxes = []                       # multiple comboBoxes are required to show dropDowns for each teacher
-        self.callingComboBoxIndex = None
+        self.teacher_combo_boxes = []              # multiple comboBoxes are required to show dropDowns for each subject
+        self.callingComboBoxIndex = None           # to identify which combo Box sent the signal or assigned a value
         self.subjectList = None
-        self.windowOpened = False
+        self.windowOpened = False                  # used as a flag to know if 'share teacher' window is open or not
+        self.sharingMade = False                   # used as a flag to know if sharing is made or not
+        self.sharedList = []                       # contains subject-name along with names of teachers who shares it
+        self.teachersSelectedForSharing = 0        # counter, responsible for enabling/disabling 'share' button
+        self.sharedTeachers = []                   # list of teachers which are selected to share same subject
+        self.sharedSubjectIndex = None             # index of the subject which gets shared between teachers
+        self.finalList = []
 
         # main widget containing dynamic contents
         self.mapSubWindow = QWidget(parent)
@@ -51,7 +57,7 @@ class MapWindow:
             # showing vertical line (to separate 2 sub windows)
             self.VLine.show()
 
-            self.windowOpened = True                                            # to indicate that sharing window is open now
+            self.windowOpened = True                                       # to indicate that sharing window is open now
 
             # getting index of the operated combo box
             self.callingComboBoxIndex = index
@@ -72,7 +78,7 @@ class MapWindow:
             # appending checkbox for each teacher
             # starting from 2 as the 1st two elements of teacher's list are 'none' and 'shared'
             for i in range(2, len(self.teachers)):
-                checkbox = QCheckBox(self.teachers[i])
+                checkbox = self.create_checkboxes(i, index)
                 self.checkBoxes.append(checkbox)
 
             # displaying the teachers list (along with their checkboxes)
@@ -83,9 +89,10 @@ class MapWindow:
 
             # for 'share' and 'cancel' buttons layout
             self.shareButtonsLayout.addStretch(1)
-            share_teacher_button = QPushButton('Share')
-            share_teacher_button.clicked.connect(self.share_subject_with)
-            self.shareButtonsLayout.addWidget(share_teacher_button)         # must be referenced to set other behaviour
+            self.share_teacher_button = QPushButton('Share')
+            self.share_teacher_button.setEnabled(False)
+            self.share_teacher_button.clicked.connect(self.share_subject_between_teachers)
+            self.shareButtonsLayout.addWidget(self.share_teacher_button)     # must be referenced to set other behaviour
 
             cancel_share_button = QPushButton('Cancel')
             cancel_share_button.clicked.connect(self.close_sharing_section)
@@ -95,7 +102,7 @@ class MapWindow:
     # helper function for 'mapping_options()'
     def create_combo_box(self, for_index):
         teacher_options_widget = QComboBox()
-        teacher_options_widget.addItems(self.teachers)                      # same list of teacher is added as each comboBox items
+        teacher_options_widget.addItems(self.teachers)            # same list of teacher is added as each comboBox items
         teacher_options_widget.currentIndexChanged.connect(lambda: self.display_sharing_section(for_index))
         return teacher_options_widget
 
@@ -132,13 +139,24 @@ class MapWindow:
             if widget:
                 widget.close()
 
+        # emptying list of teachers that are selected for sharing
+        self.sharedList = []
+
         # closing the opened window
         self.mapSubWindow.close()
 
     def close_sharing_section(self):
+        # resetting dependencies
+        self.teachersSelectedForSharing = 0
+        self.sharedTeachers = []
+        self.sharedSubjectIndex = None
+
         # setting 'None' in lieu of 'Shared' if cancel button is pressed
-        if self.windowOpened is True:
-            self.teacher_combo_boxes[self.callingComboBoxIndex].setCurrentIndex(0)
+        if self.windowOpened:
+            if self.sharingMade:
+                self.teacher_combo_boxes[self.callingComboBoxIndex].setCurrentIndex(1)
+            else:
+                self.teacher_combo_boxes[self.callingComboBoxIndex].setCurrentIndex(0)
 
             # enabling all the disabled drop-downs)
             for i in range(len(self.teacher_combo_boxes)):
@@ -167,7 +185,9 @@ class MapWindow:
             if widget:
                 widget.close()
 
+        # resetting flag after their use
         self.windowOpened = False
+        self.sharingMade = False
 
     # for disabling all the drop-downs (for selecting teacher) till sharing window is open
     def disable_combo_box(self, at_index):
@@ -177,6 +197,72 @@ class MapWindow:
     def enable_combo_box(self, at_index):
         self.teacher_combo_boxes[at_index].setEnabled(True)
 
-    # for storing the (only) shared mappings
-    def share_subject_with(self):
-        pass
+    # creating checkboxes
+    def create_checkboxes(self, index_of_teacher, for_subject):
+        checkbox = QCheckBox(self.teachers[index_of_teacher])
+        checkbox.toggled.connect(lambda: self.process_checkbox(index_of_teacher, for_subject))
+        return checkbox
+
+    # function called whenever teacher checkbox is toggled
+    def process_checkbox(self, teacher_index, for_subject):
+        # checkboxes are 2 less than elements in teacher list (as 'none' and 'Shared' are not included)
+        checkbox_index = teacher_index - 2
+        self.sharedSubjectIndex = for_subject
+
+        selected_teacher = self.teachers[teacher_index]
+        # increase or decrease number of teacher selected or checked
+        if self.checkBoxes[checkbox_index].isChecked():
+            self.teachersSelectedForSharing += 1                # increasing the counter
+            self.sharedTeachers.append(selected_teacher)        # adding teacher for sharing subject
+        else:
+            self.teachersSelectedForSharing -= 1                # decreasing the counter
+
+            # removing the teacher from the sharedTeacher list
+            for i in range(len(self.sharedTeachers)):
+                if selected_teacher == self.sharedTeachers[i]:
+                    self.sharedTeachers.remove(self.sharedTeachers[i])
+
+        # enabling 'Share' button only if more than 1 teacher is selected
+        if self.teachersSelectedForSharing > 1:
+            self.share_teacher_button.setEnabled(True)
+        else:
+            self.share_teacher_button.setEnabled(False)
+
+    # updates the list of subjects that are shared by different teacher for a semester/batch
+    def share_subject_between_teachers(self):
+        # generating a mapping in the form dictionary
+        subject = self.subjectList[self.sharedSubjectIndex]
+        mapping = {subject: self.sharedTeachers}
+
+        # update the value (ie teacher names) if mapping for the same subject already exists
+        if len(self.sharedList) > 0:  # means some mappings exist
+            subject_match_found = False
+            for each_dictionary in self.sharedList:
+                for key, values in each_dictionary.items():
+                    if subject == key:
+                        print('updating existing value')
+                        each_dictionary[key] = self.sharedTeachers      # updating the teacher names for the subject
+                        subject_match_found = True
+                        break
+
+                if subject_match_found:
+                    break                                               # stop looking for the subject
+                else:                                                   # if mapping for the subject doesn't exists
+                    print('creating new mapping')
+                    self.sharedList.append(mapping)                     # append the new mapping
+                    break
+
+        else:  # if no mapping exists
+            print('creating first mapping...')
+            self.sharedList.append(mapping)
+
+        # updating the flag to refer sharing is made
+        self.sharingMade = True
+
+        # test output
+        for i in self.sharedList:
+            print(i)
+        print()
+
+        # closing the shared window after done processing
+        self.close_sharing_section()
