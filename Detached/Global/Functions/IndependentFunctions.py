450acad5f824b5e1498b859e2cb33999fbc17bcc
Dev Vrat <itsdvvs.18@gmail.com>
2019-02-09 12:39:09 +0530

    Included a function to add time and generate output in 24-hours format

--- Detached/Global/Functions/IndependentFunctions.py
+++ Detached/Global/Functions/IndependentFunctions.py
@@ -5,6 +5,10 @@ from Detached.Global.Configurations.ConnectionEstablishment import *
    Hence, if this file doesn't exists, create manually at following location (within the project):
    Detached/Global/Configurations/LocalUsersConfigurations.py
    
+   File definition:
+   project_location = 'absolute address to the project on your machine'
+   e.g. : project_location = '/home/dev/Documents/PycharmProjects/RADAtR/'
+   
    Note that this file is not supposed to be on tracking."""
 from Detached.Global.Configurations.LocalUsersConfigurations import *
 
@@ -68,3 +72,87 @@ def get_list_of_subject_for(course, semester):
     for subject in cursor[str(course)][str(semester)]:
         sub_list.append(subject)
     return sub_list
+
+
+# function to add minutes to given time and return the result in 24-hour format
+def add_time_to(initial_time, minutes_to_add):
+    # validation of arguments
+    if minutes_to_add[0] == '-':
+        print('Cannot add negative values')
+        return -1
+    if initial_time[0] == '-':
+        print('You seems to use a watch from different world')
+        return -1
+
+    # have use in representations and conversions
+    zeros_in_hours_part = 0
+    zeros_in_minutes_part = 0
+    hour_carry = 0
+
+    if len(initial_time) == 4:                      # validation of initial time (must be of 4 digits)
+        # storing hours and minutes digits separately
+        hours_part = initial_time[0:2]
+        minutes_part = initial_time[2:4]
+
+        # getting the non zero digits only for hours part
+        for i in range(len(hours_part)):
+            if hours_part[i] == '0':
+                zeros_in_hours_part += 1
+            else:
+                break                               # stop as soon as a non-zero digit is encountered
+
+        # conversion of hours part from string to integer
+        if zeros_in_hours_part == 2:
+            hours_part = 0                          # direct assignment due to base 10 conversion issue
+        else:
+            hours_part = int(hours_part[zeros_in_hours_part:2])
+
+        # getting the non zero digits only for minutes part
+        for i in range(len(minutes_part)):
+            if minutes_part[i] == '0':
+                zeros_in_minutes_part += 1
+            else:
+                break                               # stop as soon as a non-zero digit is encountered
+
+        # conversion of minutes part from string to integer
+        if zeros_in_minutes_part == 2:
+            minutes_part = 0  # direct assignment due to base 10 conversion issue
+        else:
+            minutes_part = int(minutes_part[zeros_in_minutes_part:2])
+
+        # conversion of minutes_to_add from string to integer
+        if minutes_to_add == '00':
+            minutes_to_add = 0
+        else:
+            minutes_to_add = int(minutes_to_add)
+
+        # adding (minutes) time
+        minutes_part = minutes_part + minutes_to_add
+
+        if minutes_part >= 60:
+            hour_carry = minutes_part // 60
+            minutes_part = minutes_part % 60
+
+        # adding (hours) time
+        hours_part = hours_part + hour_carry
+
+        if hours_part >= 24:
+            hours_part = hours_part % 24
+
+        # converting numeric time value back to string
+        minutes_part = str(minutes_part)
+        hours_part = str(hours_part)
+
+        if len(hours_part) == 1:
+            hours_part = '0' + hours_part
+
+        if len(minutes_part) == 1:
+            minutes_part = '0' + minutes_part
+
+        resultant_time = hours_part + minutes_part
+        print(resultant_time)
+        return resultant_time
+
+    else:
+        print('Invalid input for initial time. Must be of 4 digits')
+        return -1
