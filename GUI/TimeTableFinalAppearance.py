d03b1a3270d3aaf792ea4a9161e434780098dda5
Dev Vrat <itsdvvs.18@gmail.com>
2019-02-09 17:56:50 +0530

    Improved TimeTable_Generate window

--- GUI/TimeTableFinalAppearance.py
+++ GUI/TimeTableFinalAppearance.py
@@ -1,6 +1,7 @@
 import sys
 from PyQt5.QtWidgets import *
 from Detached.Global.Variables.varFile1 import *
+from Detached.Global.Functions.IndependentFunctions import add_time_to
 
 
 class Timetable(QWidget):
@@ -8,31 +9,76 @@ class Timetable(QWidget):
     def __init__(self):
         super().__init__()
         self.title = 'Time Table'
-        self.MainWindowProp()
+        self.days_list = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
+        self.baseTime = '1000'
+        self.slotDuration = '60'
+        self.slots = []
 
-    def MainWindowProp(self):
         self.setWindowTitle(self.title)
-        self.setGeometry(300, 300, 800, 300)
+        self.setFixedSize(938, 480)
 
-        self.createTable()
+        # Add box layout
+        self.layout = QVBoxLayout(self)
 
-        # Add box layout, add table to box layout and add box layout to widget
-        self.layout = QVBoxLayout()
+        self.tableWidget = QTableWidget(self)
+
+        # rows creation and its properties
+        self.tableWidget.setRowCount(totalWorkingDays)
+        for row_no in range(totalWorkingDays):
+            self.tableWidget.setRowHeight(row_no, 80)
+
+        # columns creation and its properties
+        self.tableWidget.setColumnCount(maximumSlots)
+        for column_no in range(maximumSlots):
+            if column_no == lunch_slot_no - 1:
+                self.tableWidget.setColumnWidth(column_no, 50)
+            else:
+                self.tableWidget.setColumnWidth(column_no, 130)
+
+        self.tableWidget.setVerticalHeaderLabels(self.days_list)
+        self.get_next_slot(maximumSlots)
+        self.tableWidget.setHorizontalHeaderLabels(self.slots)
+
+        # creating buttons area
+        self.buttons_layout = QHBoxLayout(self)
+
+        # creating ('Previous', 'Next', 'Finalize' and 'Cancel') buttons
+        previous_button = QPushButton('Previous')
+        next_button = QPushButton('Next')
+        finalize_button = QPushButton('Finalize')
+        cancel_button = QPushButton('Cancel')
+
+        # adding buttons in a line (horizontally)
+        self.buttons_layout.addStretch(2)  # creates empty space on the left side
+        self.buttons_layout.addWidget(previous_button)
+        self.buttons_layout.addWidget(next_button)
+        self.buttons_layout.addWidget(finalize_button)
+        self.buttons_layout.addWidget(cancel_button)
+
+
+        # add table to box layout and add box layout to widget
         self.layout.addWidget(self.tableWidget)
+        self.layout.addLayout(self.buttons_layout)
         self.setLayout(self.layout)
 
-    def createTable(self):
-        # Create table
-        self.tableWidget = QTableWidget()
-        self.tableWidget.setRowCount(totalWorkingDays)
-        self.tableWidget.setColumnCount(maximumSlots)
-        self.tableWidget.move(0, 0)
-        self.tableWidget.setVerticalHeaderLabels(days_list)
-        self.tableWidget.setHorizontalHeaderLabels(time_slot)
+    def get_next_slot(self, for_n_slots):
+        for i in range(for_n_slots):
+            # representation for 'Recess' Column
+            if i == lunch_slot_no - 1:                      # as lunch_slot_no isn't index
+                next_slot_time = add_time_to(self.baseTime, self.slotDuration)
+                self.baseTime = next_slot_time
+                self.slots.append('')
+            else:
+                # storing slot starting time
+                start = self.baseTime
 
-        self.tableWidget.setItem(0, 0, QTableWidgetItem())
-        self.tableWidget.setItem(0, 1, QTableWidgetItem())
-        self.tableWidget.setItem(0, 2, QTableWidgetItem())
+                # calculating end time for the current slot
+                end = add_time_to(self.baseTime, self.slotDuration)
+                self.baseTime = end
+
+                # creating label for slots
+                column_label = start + '-' + end
+                self.slots.append(column_label)
 
 
 def main():
