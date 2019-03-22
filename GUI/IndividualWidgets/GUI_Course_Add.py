9dc1a807dc87f0a5e0b6a39a4b7f4847d5810f62
Dev Vrat <itsdvvs.18@gmail.com>
2019-03-23 00:51:05 +0530

    Improved looks, added 'Cancel' & 'Add' buttons

--- GUI/IndividualWidgets/GUI_Course_Add.py
+++ GUI/IndividualWidgets/GUI_Course_Add.py
@@ -1,22 +1,35 @@
-import sys
 from PyQt5.QtWidgets import *
+from PyQt5.QtGui import QFont
 
 
 class AddCourse(QWidget):
     def __init__(self, parent=None):
         super().__init__(parent)
 
-        # window properties
-        self.setFixedSize(600, 400)
-        self.setWindowTitle("Add Course")
-
         # layouts for the window
-        self.mainVLayout = QVBoxLayout(self)
-        self.nestedHLayout = QHBoxLayout(self)
-        self.nestedFormLayout = QFormLayout(self)
+        self.rootVerticalLayout = QVBoxLayout(self)
+
+        # creating style to apply on heading
+        self.font = QFont()
+        self.font.setBold(True)
+        self.font.setPointSize(24)
+
+        self.message = ''                   # used to show any warning or message upon the click of 'Add' button
 
         # ----------- widgets of the window ---------------
-        main_prompt = QLabel("Enter the details of the course to be added.")
+        # header part (contains heading and main prompt)
+        self.headerPart = QWidget(self)
+        self.headerPartLayout = QVBoxLayout(self.headerPart)
+        heading = QLabel("Add a Course", self.headerPart)
+        heading.setFont(self.font)
+        main_prompt = QLabel("Enter the details of the course to be added.", self.headerPart)
+        # adding widget to layout
+        self.headerPartLayout.addWidget(heading)
+        self.headerPartLayout.addWidget(main_prompt)
+
+        # content part (contains label and input fields)
+        self.contentPart = QWidget(self)
+        self.nestedFormLayout = QFormLayout(self.contentPart)
 
         self.nameField = QLineEdit()                # for 'name' input
         self.nameField.setFixedWidth(300)
@@ -34,26 +47,28 @@ class AddCourse(QWidget):
         self.nestedFormLayout.addRow(QLabel("Total Semesters"), self.semesterField)
         self.nestedFormLayout.addRow(QLabel("Starting Time (24-hours format)"), self.startTimingField)
 
-        # adding form into horizontal layout (so it could be in center)
-        self.nestedHLayout.addStretch(1)                                # for space on the left
-        self.nestedHLayout.addLayout(self.nestedFormLayout)             # displaying form in the center
-        self.nestedHLayout.addStretch(1)                                # for space on the right
+        # footer part (contains message area, 'cancel' and 'add' buttons)
+        self.footerPart = QWidget(self)
+        self.footerPartLayout = QHBoxLayout(self.footerPart)
+
+        # widgets of footer part
+        self.prompt = QLabel(self.message, self.footerPart)
+        self.cancelButton = QPushButton('Cancel', self.footerPart)
+        self.addButton = QPushButton('Add', self.footerPart)
+        # adding widgets into the footer-part layout
+        self.footerPartLayout.addWidget(self.prompt, 2)
+        self.footerPartLayout.addWidget(self.cancelButton)
+        self.footerPartLayout.addWidget(self.addButton)
+
+        # creating a horizontal line
+        self.horizontalLine = QFrame(self)
+        self.horizontalLine.setFrameShape(QFrame.HLine)
+        self.horizontalLine.setFrameShadow(QFrame.Sunken)
 
         # stacking all the widgets in the main layout
-        self.mainVLayout.addWidget(main_prompt)
-        self.mainVLayout.addStretch(1)
-        self.mainVLayout.addLayout(self.nestedHLayout)
-        self.mainVLayout.addStretch(2)
-        self.setLayout(self.mainVLayout)
-
-
-def main():
-    application = QApplication(sys.argv)
-    application.setStyle('Fusion')
-    add_course_object = AddCourse()
-    add_course_object.show()
-    sys.exit(application.exec_())
-
-
-if __name__ == '__main__':
-    main()
+        self.rootVerticalLayout.addWidget(self.headerPart)
+        self.rootVerticalLayout.addWidget(self.contentPart)
+        self.rootVerticalLayout.addStretch(5)
+        self.rootVerticalLayout.addWidget(self.horizontalLine)
+        self.rootVerticalLayout.addWidget(self.footerPart)
+        self.rootVerticalLayout.setContentsMargins(80, 10, 0, 0)
