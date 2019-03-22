e3a8101cccd614f7f36d308e1e053763b0a93e62
Dev Vrat <itsdvvs.18@gmail.com>
2019-03-22 20:33:34 +0530

    Added buttons for few more modules. Not functional yet.

--- GUI/GUI_ModulePanel.py
+++ GUI/GUI_ModulePanel.py
@@ -13,15 +13,45 @@ class LeftModulePanel:
         # layout for left bar
         self.leftBarLayout = QVBoxLayout(self.modulePanel)
 
-        # buttons on the left bar
+        """----------- buttons on the (left) Modules Panel --------------"""
+        # for timeTable module
         self.timeTableButton = QPushButton('', self.modulePanel)
         self.timeTableButton.setIcon(QIcon('Icons/timetable.png'))
         self.timeTableButton.setIconSize(QSize(40, 40))
 
-        # stacking all buttons in left bar
-        self.leftBarLayout.addStretch(1)
+        # for scheduleExam module
+        self.examScheduleButton = QPushButton('', self.modulePanel)
+        self.examScheduleButton.setIcon(QIcon('Icons/timetable.png'))
+        self.examScheduleButton.setIconSize(QSize(40, 40))
+
+        # for Admission module
+        self.admissionButton = QPushButton('', self.modulePanel)
+        self.admissionButton.setIcon(QIcon('Icons/timetable.png'))
+        self.admissionButton.setIconSize(QSize(40, 40))
+
+        # for calender (home screen)
+        self.calenderButton = QPushButton('', self.modulePanel)
+        self.calenderButton.setIcon(QIcon('Icons/timetable.png'))
+        self.calenderButton.setIconSize(QSize(40, 40))
+
+        # for miscellaneous sub-modules
+        self.unitSubModuleButton = QPushButton('', self.modulePanel)
+        self.unitSubModuleButton.setIcon(QIcon('Icons/timetable.png'))
+        self.unitSubModuleButton.setIconSize(QSize(40, 40))
+
+        # stacking all buttons inside the (left-most) panel
+        self.leftBarLayout.addStretch(5)
         self.leftBarLayout.addWidget(self.timeTableButton)
-        self.leftBarLayout.addStretch(20)
+        self.leftBarLayout.addStretch(1)
+        self.leftBarLayout.addWidget(self.examScheduleButton)
+        self.leftBarLayout.addStretch(1)
+        self.leftBarLayout.addWidget(self.admissionButton)
+        self.leftBarLayout.addStretch(1)
+        self.leftBarLayout.addWidget(self.unitSubModuleButton)
+        self.leftBarLayout.addStretch(1)
+        self.leftBarLayout.addWidget(self.calenderButton)
+        self.leftBarLayout.addStretch(30)
+
         # margin order (left, top, right, bottom)
         self.leftBarLayout.setContentsMargins(1, 0, 1, 0)
         self.leftBarLayout.setSpacing(0)
