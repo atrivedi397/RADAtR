f3ec83b640f8c8e8a21d722c2a4469861fd0f70e
Dev Vrat <itsdvvs.18@gmail.com>
2019-03-23 05:16:10 +0530

    Reduced the number import widgets components

--- GUI/IndividualWidgets/GUI_Course_Add.py
+++ GUI/IndividualWidgets/GUI_Course_Add.py
@@ -1,4 +1,7 @@
-from PyQt5.QtWidgets import *
+from PyQt5.QtWidgets import (
+    QWidget, QVBoxLayout, QHBoxLayout, QLabel, QLineEdit, QFormLayout,
+    QComboBox, QPushButton, QFrame
+)
 from PyQt5.QtGui import QFont
 
 
