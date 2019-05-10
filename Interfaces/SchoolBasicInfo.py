9b6eb5516bafed3ae88bf070ac63416bb969951a
Dev Vrat <itsdvvs.18@gmail.com>
2019-05-11 05:14:01 +0530

    Resized window, changed background color

--- Interfaces/SchoolBasicInfo.py
+++ Interfaces/SchoolBasicInfo.py
@@ -6,24 +6,30 @@ import sys
 from PyQt5.QtWidgets import (
     QApplication, QWidget,  QHBoxLayout
 )
+from Global.Styles.Colors import *
+from Interfaces.BasicInfo_Nav import BasicInfoNavSection
 
 
 # represents whole window
 class SchoolBasicInfoWindow(QWidget):
-    def __int__(self, parent=None):
+    def __init__(self, parent=None):
         super().__init__(parent)
-
-        print('hi')
 
         # title of the main window
         window_title = 'School Basic Information'
         self.setWindowTitle(window_title)
 
         # values are in pixels
-        self.windowTopMargin = 10
-        self.windowLeftMargin = 10
+        self.windowTopMargin = 30
+        self.windowLeftMargin = 50
         self.windowHeight = 600
-        self.windowWidth = 1280
+        self.windowWidth = 900
+
+        # appearance of the window (changing background as dark)
+        self.setAutoFillBackground(True)
+        self.mainPalette = self.palette()
+        self.mainPalette.setColor(self.backgroundRole(), darcula_background)
+        self.setPalette(self.mainPalette)
 
         # initializing size and the location on screen for the window
         self.setGeometry(self.windowTopMargin, self.windowLeftMargin, self.windowWidth, self.windowHeight)
@@ -42,14 +48,16 @@ class SchoolBasicInfoWindow(QWidget):
         """
 
         # navigation section widget (to be placed on the left side)
-        self.navigationSection = QWidget(self)
+        self.navigationSection = BasicInfoNavSection(self)
 
         # context section widget (to be placed on the right side)
         self.contextSection = QWidget(self)
 
         # adding above sections (i.e. navigation and context) to the main layout
-        self.mainLayout.addWidget(self.navigationSection)
-        self.mainLayout.addWidget(self.contextSection)
+        self.mainLayout.addWidget(self.navigationSection, 1)
+        self.mainLayout.addWidget(self.contextSection, 3)
+
+        self.mainLayout.setContentsMargins(0, 0, 0, 0)         # remove margins
         self.setLayout(self.mainLayout)
 
 
