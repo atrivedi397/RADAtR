43750fa32677cff37f5e9fc3a561bdb34895bf5e
Abhishek Trivedi <atrivedi397@outlook.com>
2019-05-11 03:38:56 +0530

    registration of school and admin

---
+++ school_functions/school_database_variables.py
@@ -0,0 +1,12 @@
+localhost = "localhost:27017"
+
+# database used
+school_database = "school_db"
+
+# different collections used for storing and retrieving
+admin_collection = "admin_table"
+school_info = "school_info"
+
+# school details attributes
+school_attrib = ["name", "board", "level_of_education", "classroom_no",
+                 "total_teachers", "working_days", "address", "working_hours"]
